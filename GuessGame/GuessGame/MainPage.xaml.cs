3ffb0ccf2d4d7d429972dc3053214156d657e85d
Steve Gregory <Stephencgregory@gmail.com>
2018-11-15 08:57:25 -0200

    Add NN

--- GuessGame/GuessGame/MainPage.xaml.cs
+++ GuessGame/GuessGame/MainPage.xaml.cs
@@ -21,6 +21,7 @@ namespace GuessGame
         public MainPage()
         {
             InitializeComponent();
+            answers = new List<Answer>();
         }
 
         private void OnPainting(object sender, SKPaintSurfaceEventArgs e)
@@ -56,35 +57,7 @@ namespace GuessGame
             }
 
                 skimage = surface.Snapshot();
-           //SKImage snapI = e.Surface.Snapshot();
-
-            //var x = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "FolderName");
-            //var fullpath = x + "PicName.png";
-            //File.WriteAllBytes(fullpath, pngImage.ToArray());
-            //SKBitmap bitmap = SKBitmap.Decode(fullpath);
-            //var dstInfo = new SKImageInfo(1060, 550);
-
-            //bitmap.Resize(dstInfo, SKBitmapResizeMethod.Hamming);
-            ////image.
-            //SKBitmap bitmap = SKBitmap.FromImage(skimage);
-            ////bitmap.Resize(new SKImageInfo(100, 100), SKBitmapResizeMethod.Lanczos3);
-            //var byteArray = bitmap.Bytes;
-            //Stream stream = new MemoryStream(byteArray);
-            //ImageDim.Source = ImageSource.FromStream(()=> { return stream; });
-            //using (var scaledBitmap = bitmap.Resize(new SKImageInfo(500, 100), SKBitmapResizeMethod.Lanczos3))
-            //{
-            //    using (var image = SKImage.FromBitmap(scaledBitmap))
-            //    {
-            //        using (var png = image.Encode(SKEncodedImageFormat.Png, 100))
-            //        {
-            //            File.Create("C:\\AZ\\scaled.png");
-            //            using (var filestream = File.OpenWrite("C:\\AZ\\scaled.png"))
-            //            {
-            //                png.SaveTo(filestream);
-            //            }
-            //        }
-            //    }
-            //}
+         
         }
 
         private void OnTouch(object sender, SKTouchEventArgs e)
@@ -151,5 +124,122 @@ namespace GuessGame
             }
 
         }
+
+        private List<Answer> answers;
+        private void Leaning_Click(object sender, EventArgs e)
+        {
+            if (skimage == null)
+            {
+                return;
+            }
+            SKBitmap bitmap = SKBitmap.FromImage(skimage);
+            var scaledBitmap = bitmap.Resize(new SKImageInfo(100, 100), SKBitmapResizeMethod.Triangle);
+            skimage = SKImage.FromBitmap(scaledBitmap);
+            bitmap = SKBitmap.FromImage(skimage);
+
+            SKData pngImage = skimage.Encode();
+            var byteArray = pngImage.ToArray();
+            Stream stream = new MemoryStream(byteArray);
+            ImageDim.Source = ImageSource.FromStream(() => { return stream; });
+            SKColor[] arrColors = bitmap.Pixels;
+            List<bool> boolImage = new List<bool>();
+            foreach (var item in arrColors)
+            {
+                boolImage.Add(item.ToFormsColor() == Color.Black);
+            }
+            var namePicture = NamePicture.Text;
+            if (string.IsNullOrEmpty(namePicture))
+            {
+                DisplayAlert("Error", "empty name", "ok");
+                return;
+            }
+            
+            var answer = new Answer();
+            if (answers.Any(x=>x.Name == namePicture))
+            {
+               answer = answers.FirstOrDefault(x=>x.Name == namePicture);
+                for (int i = 0; i < boolImage.Count; i++)
+                {
+                    if (boolImage[i])
+                    {
+                        answer.Weights[i] += 0.2;
+                    }
+                    else
+                    {
+                        answer.Weights[i] -= 0.05;
+                    }
+                   
+                }
+            }
+            else
+            {
+                answer.Name = namePicture;
+                foreach (var isBlackPic in boolImage)
+                {
+                    if (isBlackPic)
+                    {
+                        answer.Weights.Add(0.2);
+                    }
+                    else
+                    {
+                        answer.Weights.Add(0.001);
+
+                    }
+                }
+                answers.Add(answer);
+            }
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+
+            if (skimage == null)
+            {
+                return;
+            }
+            SKBitmap bitmap = SKBitmap.FromImage(skimage);
+            var scaledBitmap = bitmap.Resize(new SKImageInfo(100, 100), SKBitmapResizeMethod.Triangle);
+            skimage = SKImage.FromBitmap(scaledBitmap);
+            bitmap = SKBitmap.FromImage(skimage);
+
+            SKData pngImage = skimage.Encode();
+            var byteArray = pngImage.ToArray();
+            Stream stream = new MemoryStream(byteArray);
+            ImageDim.Source = ImageSource.FromStream(() => { return stream; });
+            SKColor[] arrColors = bitmap.Pixels;
+            List<bool> boolImage = new List<bool>();
+            foreach (var item in arrColors)
+            {
+                boolImage.Add(item.ToFormsColor() == Color.Black);
+            }
+
+            string answer = "no";
+            double resultamountWeights =0;
+            foreach (var item in answers)
+            {
+                double amountWeights = 0.1;
+                for (int i = 0; i < item.Weights.Count; i++)
+                {
+                    double valueImage = boolImage[i] ? 1 : 0;
+                    amountWeights += valueImage * item.Weights[i];
+                }
+                if (resultamountWeights< amountWeights)
+                {
+                    resultamountWeights = amountWeights;
+                    answer = item.Name;
+                }
+            }
+            DisplayAlert("Answer", answer,"ok");
+        }
+    }
+    public class Answer
+    {
+        public Answer()
+        {
+            Weights = new List<double>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<double> Weights { get; set; }
     }
 }
